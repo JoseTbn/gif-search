1b329486389a21b0c482d4f20474b846843bafbd
JoseTbn <82665551+JoseTbn@users.noreply.github.com>
2021-10-07 00:11:09 +0200

    added an hover effect

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,28 @@
 import React , { useState, useEffect  } from 'react'
 import '../App.css';
 import axios from 'axios';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 import SearchForm from './SearchForm';
 import GifList from './GifList';
 
 function App() {
   
+  AOS.init();
+
   const [data, setdata] = useState([]);
   const [query, setQuery] = useState('anime');
   const [isLoading, setIsLoading] = useState(true);
 
   const performSearch = (value) => setQuery(value)
+
+  useEffect(() => {
+    AOS.init({
+      duration : 2000
+    });
+    AOS.refresh();
+  }, []);
 
   useEffect( () => {
     axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=RFLdpkjUGFesFP13u7kk4ANpcQ5DJs2W`)
@@ -24,7 +35,7 @@ function App() {
     <>
       <div className="main-header">
         <div className="inner">
-          <h1 className="main-title">Gif Search</h1>
+          <h1 className="main-title"  data-aos="fade-right">Gif Search</h1>
           <SearchForm onSearch={performSearch} />
         </div>
       </div>
